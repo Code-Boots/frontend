e22200d813c7b4114eb5ae2546dcc061d2fa1f70
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-26 00:11:27 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import { Card } from "@chakra-ui/react";
 import Cards from "./components/Cards";
 import CreditScoreMeter from "./components/CreditScoreMeter";
+import Speedometer from "./components/SpeedoMeter";
 
 export default function Home() {
   return (
     <>
       {/* <CreditScoreMeter score="900"/> */}
-      <Cards/>
+      {/* <Cards/> */}
+      <Speedometer speed={'700'} maxSpeed={'900'}/>
     </>
   )
 }
