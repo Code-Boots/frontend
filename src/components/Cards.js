af0cd381f5b43e50a166e7d1513e13bbc32b3035
Biswarghya Biswas <iambiswarghya@gmail.com>
2023-03-26 05:13:47 +0530

    updated

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,7 +8,7 @@ export default function Cards() {
       <Card w={'sm'} shadow="md" rounded={'lg'}>
           <Stack direction={'row'} >
               <Box>
-                  <Image src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60' w={'100px'} h={'100px'}/>
+                  <Image src='https://i.pinimg.com/originals/f8/b0/a2/f8b0a277e663688f577cf09101d1d1fe.gif' w={'100px'} h={'100px'}/>
               </Box>
               <Box p="3">
                   <Heading size='md'>Credit Cards</Heading>
@@ -24,7 +24,7 @@ export default function Cards() {
       <Card w={'sm'} shadow="md" rounded={'lg'}>
           <Stack direction={'row'} >
               <Box>
-                  <Image src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60' w={'100px'} h={'100px'}/>
+                  <Image src='https://cdn-images-1.medium.com/max/1200/1*evpRkULzf2s5JYoA3bTDcw.png' w={'100px'} h={'100px'}/>
               </Box>
               <Box p="3">
                   <Heading size='md'>Loans</Heading>
@@ -38,4 +38,4 @@ export default function Cards() {
     
     </>
   )
-}1
+}
