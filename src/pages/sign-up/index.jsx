44dc472e8b6cc211aaa8e7d628587a30ac7f1fb9
subh-23 <subhodiproy281@gmail.com>
2023-03-25 22:19:49 +0530

    form added successfully

--- src/pages/sign-up/index.jsx
+++ src/pages/sign-up/index.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const SignUp = () => {
+const index = () => {
+  const [fullName, setFullName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [dob, setDob] = useState("");
+  const [pan, setPan] = useState("");
+
+  const submitHandler = () => {
+    if (fullName === "" || phone === "" || dob === "" || pan === "") {
+      alert("Fill all the fields");
+      return;
+    }
+  };
+
   return (
-    <div>SignUp</div>
-  )
-}
+    <div className="flex items-center	justify-center h-screen">
+      <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="space-y-6">
+          <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+            Fill up your details
+          </h5>
+          <div>
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your name
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={fullName}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              placeholder="Imon Roy"
+              onChange={(e) => {
+                setFullName(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="phone"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your phone no
+            </label>
+            <input
+              type="text"
+              value={phone}
+              id="phone"
+              placeholder="+91 9837467300"
+              onChange={(e) => {
+                setPhone(e.target.value);
+              }}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="dob"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your date-of-birth
+            </label>
+            <input
+              type="date"
+              id="dob"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              value={dob}
+              onChange={(e) => {
+                setDob(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="pan"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your PAN no.
+            </label>
+            <input
+              type="text"
+              name="pan"
+              id="pan"
+              placeholder="EZOPR3298Q"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              onChange={(e) => {
+                setPan(e.target.value);
+              }}
+            />
+          </div>
+          <div
+            onClick={submitHandler}
+            className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer"
+          >
+            Save your details
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SignUp
+export default index;
