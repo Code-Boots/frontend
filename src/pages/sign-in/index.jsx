b8a1b6f0cbdb4a085048b330803e9b84495d3ada
subh-23 <subhodiproy281@gmail.com>
2023-03-26 00:51:27 +0530

    register form added successfully

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+const check = false;
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return <div>{check ? <h1>True</h1> : <h1>False</h1>}</div>;
 };
 
 export default SignIn;
